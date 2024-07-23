ae045b085eaa96f12f27985f0425e41d5c3eb339
Eugene Ihde <eugene.ihde@live.de>
2024-07-23 23:09:31 +0200

    add statistics page

---
+++ src/app/dashboard/statistics/page.tsx
@@ -0,0 +1,13 @@
+'use client'
+
+import { Typography } from '@mui/material'
+
+const StatisticsPage = () => {
+  return (
+    <>
+      <Typography variant='h1'>Coming soon…</Typography>
+    </>
+  )
+}
+
+export default StatisticsPage
